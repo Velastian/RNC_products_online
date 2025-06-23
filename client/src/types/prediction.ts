b25dd052350ebb63930905a93bdcfd5e901a62d7
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-06-23 13:25:43 -0600

    Feat: agregando tabla para mostrar las predicciones

---
+++ client/src/types/prediction.ts
@@ -0,0 +1,16 @@
+export interface Prediction {
+  id: string;
+  imagen: string;
+  bicycle: number;
+  cabinet: number;
+  chair: number;
+  coffeMaker: number;
+  fan: number;
+  kettle: number;
+  lamp: number;
+  mug: number;
+  sofa: number;
+  stapler: number;
+  table: number;
+  toaster: number;
+};
